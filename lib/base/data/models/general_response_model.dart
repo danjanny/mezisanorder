6ff3ff9e0b5445e4c43024b4ca38c198bf4ea1e0
ridhadanjanny <ridhadanjanny.mail@gmail.com>
2024-07-21 12:57:14 +0700

    update cubit login param using LoginRequest

--- lib/base/data/models/general_response_model.dart
+++ lib/base/data/models/general_response_model.dart
@@ -1,30 +1,9 @@
-class GeneralResponse<T> {
+class GeneralResponse {
   final String responseCode;
   final String responseMessage;
-  final T? data;
 
   GeneralResponse({
     required this.responseCode,
     required this.responseMessage,
-    this.data,
   });
-
-  factory GeneralResponse.fromJson(
-      Map<String, dynamic> json,
-      T Function(Map<String, dynamic>) createDataModel,
-      ) {
-    return GeneralResponse<T>(
-      responseCode: json['responseCode'],
-      responseMessage: json['responseMessage'],
-      data: json['data'] != null ? createDataModel(json['data']) : null,
-    );
-  }
-
-  Map<String, dynamic> toJson(Map<String, dynamic> Function(T) transformDataModel) {
-    return {
-      'responseCode': responseCode,
-      'responseMessage': responseMessage,
-      'data': data != null ? transformDataModel(data!) : null,
-    };
-  }
 }

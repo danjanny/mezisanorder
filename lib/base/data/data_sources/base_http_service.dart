7223181620b61feaacd284f3361f236e24a29206
ridhadanjanny <ridhadanjanny.mail@gmail.com>
2024-11-03 20:38:29 +0700

    integrate chucker on base_http_service.dart, enhance headers to use content-type form data, and body adjusted too

--- lib/base/data/data_sources/base_http_service.dart
+++ lib/base/data/data_sources/base_http_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:chucker_flutter/chucker_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,17 +13,24 @@ abstract class BaseHttpService {
     if (queryParams != null) {
       uri = uri.replace(queryParameters: queryParams);
     }
-    final response = await http.get(uri);
+
+    // final client = ChuckerHttpClient(http.Client());
+    // final response = await client.get(uri, headers: await _generateHeaders());
+
+    final client = ChuckerHttpClient(http.Client());
+    final response = await client.get(uri);
     return response;
   }
 
   Future<http.Response> fetchPost(String path,
       {required Map<String, dynamic> body}) async {
     var uri = Uri.parse(baseUrl + path);
-    final response = await http.post(
+
+    final client = ChuckerHttpClient(http.Client());
+    final response = await client.post(
       uri,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: body.entries.map((e) => '${Uri.encodeQueryComponent(e.key)}=${Uri.encodeQueryComponent(e.value)}').join('&'),
     );
     return response;
   }

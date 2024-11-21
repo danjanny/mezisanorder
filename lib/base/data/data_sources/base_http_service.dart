de6afe97d8fb328f7ffa7ac7bcfd196ec33932d9
Ridha Danjanny <ridhadanjanny.mail@gmail.com>
2024-11-21 23:45:00 +0700

    [Enhancement] use form-data as request body type & fix chucker that can show form-data too

--- lib/base/data/data_sources/base_http_service.dart
+++ lib/base/data/data_sources/base_http_service.dart
@@ -22,16 +22,65 @@ abstract class BaseHttpService {
     return response;
   }
 
-  Future<http.Response> fetchPost(String path,
-      {required Map<String, dynamic> body}) async {
+  // Future<http.Response> fetchPost(String path,
+  //     {required Map<String, dynamic> body}) async {
+  //   var uri = Uri.parse(baseUrl + path);
+  //
+  //   final client = ChuckerHttpClient(http.Client());
+  //   final response = await client.post(
+  //     uri,
+  //     headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+  //     body: body.entries.map((e) => '${Uri.encodeQueryComponent(e.key)}=${Uri.encodeQueryComponent(e.value)}').join('&'),
+  //   );
+  //   return response;
+  // }
+
+  Future<http.Response> fetchPost(String path, {Map<String, dynamic>? body}) async {
     var uri = Uri.parse(baseUrl + path);
 
-    final client = ChuckerHttpClient(http.Client());
-    final response = await client.post(
-      uri,
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      body: body.entries.map((e) => '${Uri.encodeQueryComponent(e.key)}=${Uri.encodeQueryComponent(e.value)}').join('&'),
-    );
+    final client = CustomChuckerHttpClient(http.Client());
+    final request = http.MultipartRequest('POST', uri);
+
+    if (body != null) {
+      body.forEach((key, value) {
+        if (value is String) {
+          request.fields[key] = value;
+        } else if (value is List<int>) {
+          request.files.add(http.MultipartFile.fromBytes(key, value));
+        } else {
+          request.fields[key] = value.toString();
+        }
+      });
+    }
+
+    final streamedResponse = await client.send(request);
+    final response = await http.Response.fromStream(streamedResponse);
+
+    return response;
+  }
+
+}
+
+class CustomChuckerHttpClient extends http.BaseClient {
+  final http.Client _inner;
+
+  CustomChuckerHttpClient(this._inner);
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+    if (request is http.MultipartRequest) {
+      final fields = request.fields;
+      final files = request.files.map((file) => file.filename).toList();
+      print('Form Data Fields: $fields');
+      print('Form Data Files: $files');
+    }
+
+    final response = await ChuckerHttpClient(_inner).send(request);
+
+    // Log the response if needed
+    // final responseBody = await response.stream.bytesToString();
+    // print('Response: $responseBody');
+
     return response;
   }
 }
